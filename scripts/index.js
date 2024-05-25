2f4b629b68954063992c6f1d24cee60a61203e23
Henry Cristian Cuesta Vega <thechosenone.cc@gmail.com>
2024-05-25 13:12:55 -0500

    formulario timeout v 0.1

--- scripts/index.js
+++ scripts/index.js
@@ -51,20 +51,66 @@ const baseDeDatos = {
 // 5) En caso de que los datos ingresados sean correctos, se deberá ocultar el formulario y mostrar
 // un mensaje de bienvenida al sitio.
 
-/* 
+/*
 TIPS:
   - Puedes averiguar acerca de la manera de validar el formato de un email utilizando Javascript, buscando
     en internet frases como "Validar email con Javascript o similar".
 
   - Recuerda que puedes seleccionar y manipular los elementos del archivo index.html, usando los
-    recursos que Javascript te ofrece para ello. Además, en el archivo styles.css tiene algunas clases y 
+    recursos que Javascript te ofrece para ello. Además, en el archivo styles.css tiene algunas clases y
     estilos predefinidos para ayudarte a completar la actividad.
 
   - También te dejamos algunos mensajes que te pueden ser de utilidad:
-  
+
    Mensaje de error => <small>Alguno de los datos ingresados son incorrectos</small>
 
    Mensaje de bienvenida => "<h1> Bienvenido al sitio 😀 </h1>";
 
    ¡Manos a la obra!
  */
+const leerEmail = function (email) {
+  return /^[\w \_\.]{4,}@[a-z]{3,}\.[a-z]{2,3}$/.test(email);
+};
+const leerClave = function (password) {
+  return password.length > 4;
+};
+const $email = document.getElementById("email-input");
+$email.setAttribute("autocomplete", true);
+$email.setAttribute("required", true);
+
+const $password = document.getElementById("password-input");
+$password.setAttribute("required", true);
+
+const loader = document.getElementById("loader");
+const divErrores = document.getElementById("error-container");
+const btnLogin = document.querySelector("button.login-btn");
+
+btnLogin.addEventListener("click", (e) => {
+  e.preventDefault();
+  const errores = [];
+  if (!leerEmail($email.value)) {
+    //alert(`Email incorrecto: ${$email.value}`);
+    errores.push(`Email incorrecto:`);
+  }
+  if (!leerClave($password.value)) {
+    alert(`Clave no válida: ${$password.value}`);
+    errores.push(`Contraseña no aceptada: ${$password.value}`);
+  }
+  if (errores.length > 0) {
+    divErrores.style.flexDirection = "column";
+    divErrores.style.alignItems = "flex-start";
+    errores.map((err) => {
+      let error = document.createElement("small");
+      let errorText = document.createTextNode(`${err}`);
+      error.appendChild(errorText);
+      divErrores.append(error);
+    });
+    divErrores.classList.remove("hidden");
+  }
+
+  function iniciarSesion() {}
+  loader.classList.remove("hidden");
+  let espera = setTimeout(() => {
+    loader.classList.add("hidden");
+  }, 3000);
+});
